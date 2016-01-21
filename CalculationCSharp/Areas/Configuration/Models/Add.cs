a51194343626fe6e55c4f4139ebf3656abc6350a
Jayson Herbert <jaysonherbert@hotmail.co.uk>
2016-01-21 21:35:55 +0000

    COnfig CHanges

--- CalculationCSharp/Areas/Configuration/Models/Add.cs
+++ CalculationCSharp/Areas/Configuration/Models/Add.cs
@@ -7,10 +7,12 @@ namespace CalculationCSharp.Areas.Configuration.Models
 {
     public class Add
     {
-        public Double Input1 { get; set;}
+        public Double Input1 { get; set; }
+        public Double Input2 { get; set; }
 
-        public Double AddFunction (double Input1)
+        public Double AddFunction (double Input1, double Input2)
         {
+            double total = Input1 + Input2;
             return Input1;
         }
     }
--- CalculationCSharp/Scripts/Configuration/Configuration.js
+++ CalculationCSharp/Scripts/Configuration/Configuration.js
@@ -2,9 +2,11 @@
 var $BTN = $('#export-btn');
 var $BTNSave = $('#save-btn');
 var $EXPORT = $('#export');
+var $MODAL = $('#modal-container');
 
 $('.table-add').click(function () {
-    var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+    var $row = $(this).parents('tr');
+    var $clone = $row.clone(true);
     $TABLE.find('table').append($clone);
 });
 
@@ -27,97 +29,56 @@ $('.table-down').click(function () {
 jQuery.fn.pop = [].pop;
 jQuery.fn.shift = [].shift;
 
-//$BTN.click(function () {
-//    var $rows = $TABLE.find('tr:not(:hidden)');
-//    var $input = $rows.find("input");
-//    var headers = [];
-//    var data = [];
-//    // Get the headers (add special header logic here)
-//    $($rows.shift()).find('th:not(:empty)').each(function () {
-//        headers.push($(this).text().toLowerCase());
-//    });
+//Build and Submit JSON
+$BTN.click(function () {
+    var $rows = $TABLE.find('tr:not(:hidden)');
+    var $input = $rows.find("input");
+    var headers = [];
+    var data = [];
+    // Get the headers (add special header logic here)
+    $($rows.shift()).find('th:not(:empty)').each(function () {
+        headers.push($(this).text().toLowerCase());
+    });
 
-//    // Turn all existing rows into a loopable array
-//    $rows.each(function () {
-//        var $td = $(this).find('tr :input');
-//        var h = {};
+    // Turn all existing rows into a loopable array
+    $rows.each(function () {
+        var $td = $(this).find('tr :input');
+        var h = {};
 
-//        var features = {};    // Create empty javascript object
-//        $input.each(function () {           // Iterate over inputs
-//            features[$(this).attr('name')] = $(this).val();           
-//        });
-//        data.push(features);// Add each to features object
-//    });
+        var features = {};    // Create empty javascript object
+        $input.each(function () {           // Iterate over inputs
+            features[$(this).attr('name')] = $(this).val();           
+        });
+        data.push(features);// Add each to features object
+    });
 
-//    var json = JSON.stringify(data); // Stringify to create json object (requires json2 library)
+    var json = JSON.stringify(data); // Stringify to create json object (requires json2 library)
 
-//    $.ajax({
+    $.ajax({
 
-//        type: 'POST',
-//        dataType: 'text',
-//        url: "Index",
-//        data: "jsonOfLog=" + json,
-//        success: function (data) {
-//           $("#table").html(data);
-//        },
-//        error: function (xhr, ajaxOptions, thrownError) {
-//            console && console.log("request failed");
-//        },
+        type: 'POST',
+        dataType: 'text',
+        url: "Index",
+        data: "jsonOfLog=" + json,
+        success: function (data) {
+           $("#table").html(data);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console && console.log("request failed");
+        },
 
-//        processData: false,
-//        async: false
-//    });
-//});
+        processData: false,
+        async: false
+    });
+});
 
-$BTNSave.click(function(){
-
-})
-
-//$(document).ready(function () {
-//    //Helper function to keep table row from collapsing when being sorted
-//    var fixHelperModified = function (e, tr) {
-//        var $originals = tr.children();
-//        var $helper = tr.clone();
-//        $helper.children().each(function (index) {
-//            $(this).width($originals.eq(index).width())
-//        });
-//        return $helper;
-//    };
-
-//    //Make diagnosis table sortable
-//    $("#table tbody").sortable({
-//        helper: fixHelperModified,
-//        stop: function (event, ui) { renumber_table('#table') }
-//    }).disableSelection();
-
-//    //Delete button in table rows
-//    $('table-editable').on('click', '.btn-delete', function () {
-//        tableID = '#' + $(this).closest('table-editable').attr('id');
-//        r = confirm('Delete this item?');
-//        if (r) {
-//            $(this).closest('tr').remove();
-//            renumber_table(tableID);
-//        }
-//    });
-//});
-
-////Renumber table rows
-//function renumber_table(tableID) {
-//    $(tableID + " tr").each(function () {
-//        count = $(this).parent().children().index($(this)) + 1;
-//        $(this).find('.priority').html(count);
-//    });
-//}
-
-$(function()
-{
-    var arrLinks = $('#table td:input').map(function () {
-        return $(this).val();
-    }).get();
+//Autocomplete
+$("input[name=Name]").focus(function () {
+    var values = $("input[id='task']")
+          .map(function () { return $(this).val(); }).get();
+    
     $('input[name=Name]').autocomplete({
-        source: arrLinks,
-        create: function () {
-            $(this).data("item.autocomplete", item)._renderItem = arrLinks;
-        }
+        source: values
     })
-})
+
+});
