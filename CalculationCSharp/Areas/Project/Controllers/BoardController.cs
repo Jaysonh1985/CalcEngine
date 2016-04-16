6b1efeee024ec9228591a45ff909a598d1f7a8eb
Jayson Herbert <jaysonherbert@hotmail.co.uk>
2016-04-16 15:39:26 +0100

    Front End added to kanban

--- CalculationCSharp/Areas/Project/Controllers/BoardController.cs
+++ CalculationCSharp/Areas/Project/Controllers/BoardController.cs
@@ -5,16 +5,48 @@ using System.Web;
 using System.Web.Caching;
 using System.Web.Mvc;
 using CalculationCSharp.Models;
-
+using System.Net;
 
 namespace CalculationCSharp.Areas.Project.Controllers
 {
     public class BoardController : Controller
     {
-        [HttpGet]
+        private CalculationDBContext db = new CalculationDBContext();
+
+        // GET: Project/ProjectBoards
         public ActionResult Index()
         {
+
+            ViewData["H1"] = "Project Management";
+            ViewData["P1"] = "";
+
+            return View(db.ProjectBoard.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Board()
+        {
+            ViewData["H1"] = "Project Board";
+            ViewData["P1"] = "";
             return View();
-        }                      
+        }
+
+
+        // GET: Project/ProjectBoards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectBoard projectBoard = db.ProjectBoard.Find(id);
+            if (projectBoard == null)
+            {
+                return HttpNotFound();
+            }
+            db.ProjectBoard.Remove(projectBoard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
