6f71f044e15b88d1337df8b7c8a2d464617b765b
Jayson Herbert <jaysonherbert@hotmail.co.uk>
2016-05-30 10:12:05 +0100

    Applied long press to project management app

--- CalculationCSharp/AppScript/main.js
+++ CalculationCSharp/AppScript/main.js
@@ -1,6 +1,6 @@
 ﻿// application global namespace
 var sulhome = sulhome || {};
-sulhome.kanbanBoardApp = angular.module('kanbanBoardApp', ['ui.bootstrap', 'ngRoute', 'ui.sortable', 'gridster','dndLists'])
+sulhome.kanbanBoardApp = angular.module('kanbanBoardApp', ['ui.bootstrap', 'ngRoute', 'ui.sortable', 'gridster','dndLists','pr.longpress'])
     .config(function($routeProvider){
 
         $routeProvider.when('/Maths', {
