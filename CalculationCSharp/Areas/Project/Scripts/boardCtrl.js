ff26ae530556cd3bf1ae4331c094f33a6b9e1dd6
Jayson Herbert <jaysonherbert@hotmail.co.uk>
2016-06-03 20:40:00 +0100

    Decouple project board from server

--- CalculationCSharp/Areas/Project/Scripts/boardCtrl.js
+++ CalculationCSharp/Areas/Project/Scripts/boardCtrl.js
@@ -54,12 +54,16 @@
                  };
      };
 
-     $scope.AddButtonClick = function AddStory() {
+     $scope.AddButtonClick = function AddStory(ID) {
          $scope.isLoading = true;
-         boardService.moveStory(1,1, "Add", "1","false").then(function (data) {
-             $scope.isLoading = false;
-             boardService.sendRequest();
-         }, onError);
+         storyID = $scope.columns[ID].Stories.length;
+         $scope.columns[ID].Stories.push({
+             Name: 'New',
+             Description: '',
+             AcceptanceCriteria: '',
+             ID: storyID
+         });
+
      };
 
 
@@ -78,150 +82,63 @@
          }, onError);
      };
 
-    // $scope.UpdateButtonClick = function (size) {
+     $scope.UpdateButtonClick = function (size, index) {
 
-    //    $scope.Name = this.story.Name;
-    //    $scope.Description = this.story.Description;
-    //    $scope.AcceptanceCriteria = this.story.AcceptanceCriteria;
-    //    $scope.Moscow = this.story.Moscow;
-    //    $scope.Timebox = this.story.Timebox;
-    //    $scope.User = this.story.User;
-    //    $scope.ID = this.story.Id;
-    //    $scope.Tasks = this.story.Tasks;
-    //    $scope.Comments = this.story.Comments;
-    //    $scope.colID = this.col.Id;
+        $scope.index = index;
+        $scope.Name = this.story.Name;
+        $scope.Description = this.story.Description;
+        $scope.AcceptanceCriteria = this.story.AcceptanceCriteria;
+        $scope.Moscow = this.story.Moscow;
+        $scope.Timebox = this.story.Timebox;
+        $scope.User = this.story.User;
+        $scope.ID = this.story.ID;
+        $scope.Tasks = this.story.Tasks;
+        $scope.Comments = this.story.Comments;
+        $scope.colID = this.$parent.index;
 
-    //    var modalInstance = $uibModal.open({
-    //        animation: true,
-    //        templateUrl: '/Areas/Project/Scripts/updateModal.html',
-    //        scope:$scope,
-    //        controller: function ($scope, $uibModalInstance, Name, Description, AcceptanceCriteria, Moscow, Timebox, User, ID, Tasks, Comments, colID) {
-    //            $scope.Name = Name;
-    //            $scope.Description = Description;
-    //            $scope.AcceptanceCriteria = AcceptanceCriteria;
-    //            $scope.Moscow = Moscow;
-    //            $scope.Timebox = Timebox;
-    //            $scope.User = User;
-    //            $scope.Tasks = Tasks;
-    //            $scope.Comments = Comments;
-               
-    //            $scope.ID = ID;
-                
-    //            $scope.colID = colID;
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/Areas/Project/Scripts/updateModal.html',
+            scope:$scope,
+            controller: 'storyCtrl',
+            size: size,
+            resolve: {
+                Name: function () { return $scope.Name },
+                Description: function () { return $scope.Description; },
+                AcceptanceCriteria: function () { return $scope.AcceptanceCriteria; },
+                Moscow: function () { return $scope.Moscow; },
+                Timebox: function () { return $scope.Timebox;},
+                User: function () { return $scope.User; },
+                ID: function () { return $scope.ID },
+                Tasks: function () { return $scope.Tasks },
+                Comments: function () { return $scope.Comments},
+                colID: function () { return $scope.colID; }
+            }
+        });
 
-    //            $scope.DeleteButtonClick = function AddStory() {
-    //                $scope.isLoading = true;
-    //                boardService.moveStory($scope.ID, $scope.colID, "Delete", "1","false").then(function (data) {
-    //                    $scope.isLoading = false;
-    //                    boardService.sendRequest();
-    //                }, onError);
-    //                $uibModalInstance.dismiss('cancel')
-    //            };
 
-    //            $scope.selected = {
-    //                Tasks: [{
-    //                    TaskName: "",
-    //                    TaskUser: "",
-    //                    RemainingTime: "",
-    //                    Status: ""
-    //                }]
-    //            };
+
+        modalInstance.result.then(function (selectedItem) {
+
+            $scope.columns[$scope.colID].Stories[$scope.index].ID = selectedItem.ID;
+            $scope.columns[$scope.colID].Stories[$scope.index].AcceptanceCriteria = selectedItem.AcceptanceCriteria;
+            $scope.columns[$scope.colID].Stories[$scope.index].Comments = selectedItem.Comments;
+            $scope.columns[$scope.colID].Stories[$scope.index].Description = selectedItem.Description;
+            $scope.columns[$scope.colID].Stories[$scope.index].Moscow = selectedItem.Moscow;
+            $scope.columns[$scope.colID].Stories[$scope.index].Name = selectedItem.Name;
+            $scope.columns[$scope.colID].Stories[$scope.index].Timebox = selectedItem.Timebox;
+            $scope.columns[$scope.colID].Stories[$scope.index].User = selectedItem.User;
+            $scope.columns[$scope.colID].Stories[$scope.index].Tasks = selectedItem.Tasks;
+            $scope.columns[$scope.colID].Stories[$scope.index].Comments = selectedItem.Comments;
             
-    //            if (Tasks !== null)
-    //            {
-    //                $scope.selected.Tasks = ($scope.Tasks);
-    //            }
-                    
-    //            $scope.addItem = function () {
-    //                $scope.selected.Tasks.push({
-    //                    TaskName: "",
-    //                    TaskUser: "",
-    //                    RemainingTime: "",
-    //                    Status: ""
-    //                });
-    //            },
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 
-    //            $scope.removeItem = function (index) {
-    //                $scope.selected.Tasks.splice(index, 1);
-    //            },
-
-    //            $scope.Comments = [];
-
-    //            if (Comments !== null) {
-    //                $scope.Comments = (Comments);
-    //            }
-
-    //            $scope.btn_add = function () {
-    //                if ($scope.txtcomment != '') {
-    //                    $scope.Comments.push({
-    //                        CommentName:$scope.txtcomment});
-    //                    $scope.txtcomment = "";
-    //                }
-    //            }
-
-    //            $scope.remItem = function ($index) {
-    //                $scope.Comments.splice($index, 1);
-    //            }
-
-    //            //Click OK
-    //            $scope.ok = function () {
-
-
-                    
-    //                $scope.selected = {
-    //                    Name: $scope.Name,
-    //                    Description: $scope.Description,
-    //                    AcceptanceCriteria: $scope.AcceptanceCriteria,
-    //                    Moscow: $scope.Moscow,
-    //                    Timebox: $scope.Timebox,
-    //                    User: $scope.User,
-    //                    Tasks: $scope.selected.Tasks,
-    //                    Comments: $scope.Comments
-    //                };
-                    
-
-
-    //                boardService.moveStory($scope.ID, $scope.colID, "Edit", $scope.selected, "false", $scope.columns).then(function (data) {
-    //                    $scope.isLoading = false;
-    //                    boardService.sendRequest();
-    //                }, onError);
-
-    //                $uibModalInstance.close($scope.selected.Name);
-    //            };
-
-    //            $scope.cancel = function () {
-    //                $uibModalInstance.dismiss('cancel');
-    //            };
-    //        },
-    //        size: size,
-    //        resolve: {
-    //            Name: function () { return $scope.Name },
-    //            Description: function () { return $scope.Description; },
-    //            AcceptanceCriteria: function () { return $scope.AcceptanceCriteria; },
-    //            Moscow: function () { return $scope.Moscow; },
-    //            Timebox: function () { return $scope.Timebox;},
-    //            User: function () { return $scope.User; },
-    //            ID: function () { return $scope.ID },
-    //            Tasks: function () { return $scope.Tasks },
-    //            Comments: function () { return $scope.Comments},
-    //            colID: function () { return $scope.colID; }
-    //        }
-
-
-    //    });
-
-
-
-    //    modalInstance.result.then(function (selectedItem) {
-    //        $scope.selected = selectedItem;
-    //    }, function () {
-    //        $log.info('Modal dismissed at: ' + new Date());
-    //    });
-    //};
-
-    // $scope.toggleAnimation = function () {
-    //     $scope.animationsEnabled = !$scope.animationsEnabled;
-    // };
+     $scope.toggleAnimation = function () {
+         $scope.animationsEnabled = !$scope.animationsEnabled;
+     };
 
    
 
@@ -238,3 +155,5 @@
 
     init();
 });
+
+
