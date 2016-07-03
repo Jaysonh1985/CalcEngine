aa033adb5e2abe30db1a69120d0178fb811e4219
Jayson Herbert <jaysonherbert@hotmail.co.uk>
2016-07-03 15:19:03 +0100

    4524

--- CalculationCSharp/App_Data/CalculationDB/dbo/Tables/Codes.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Codes] (
-    [Id]    INT            IDENTITY (1, 1) NOT NULL,
-    [Group] NVARCHAR (MAX) NULL,
-    [Code]  NVARCHAR (MAX) NULL,
-    [Value] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Codes] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
