65764eee334233554ac9ca9ed54f0dc2d2258191
jayso <jayso@DESKTOP-498PN9N>
2016-08-14 14:19:09 +0100

    Custom Validations

--- CalculationCSharp/Areas/Configuration/Scripts/Custom Validations/CustomValidations.js
+++ CalculationCSharp/Areas/Configuration/Scripts/Custom Validations/CustomValidations.js
@@ -1,31 +1,4 @@
-﻿sulhome.kanbanBoardApp.directive('integer', function() {
-    return {
-        require: 'ngModel',
-        scope: true,
-        link: function (scope, elm, attrs, ctrl) {
-
-            var Test = []
-
-            Test = scope;
-            
-            ctrl.$validators.integer = function (modelValue, viewValue, scope) {
-
-                var Test = []
-
-                Test = scope;
-
-                if (ctrl.$isEmpty(modelValue)) {
-                    // consider empty models to be valid
-                    return true;
-                }
-                // it is invalid
-                return false;
-            };
-        }
-    };
-});
-
-sulhome.kanbanBoardApp.directive('uibModalWindow', function () {
+﻿sulhome.kanbanBoardApp.directive('uibModalWindow', function () {
     return {
         restrict: 'EA',
         link: function(scope, element) {
@@ -33,3 +6,4 @@ sulhome.kanbanBoardApp.directive('uibModalWindow', function () {
         }
     }  
 });
+
--- CalculationCSharp/Areas/Configuration/Scripts/logicCtrl.js
+++ CalculationCSharp/Areas/Configuration/Scripts/logicCtrl.js
@@ -11,9 +11,40 @@
         $scope.Logic.splice(index, 1);
     },
 
+    $scope.OperatorClauseValidations = function OperatorClauseValidations(form) {
+
+        $scope.validations = [];
+
+        
+
+        angular.forEach($scope.Logic, function (value, key, obj) {
+
+            var AttName = 'Operator_' + key;
+
+            form[AttName].$setValidity("integer", true);
+
+            if ($scope.Logic[key].Operator != "" && $scope.Logic[key].Operator != null) {
+
+                if ($scope.Logic[key + 1] == null) {
+
+                    form[AttName].$setValidity("integer", false);
+
+                }
+
+            }
+        })
+    }
+
+
     //Click OK
-    $scope.ok = function () {
-        $uibModalInstance.close($scope.Logic);
+    $scope.ok = function (form) {
+
+        $scope.OperatorClauseValidations(form);
+
+        if (form.$valid == true) {
+            $uibModalInstance.close($scope.Logic);
+        };
+
     };
 
     $scope.cancel = function () {
