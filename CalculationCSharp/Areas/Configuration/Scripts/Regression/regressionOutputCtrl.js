8a30d2f2863c5bc4f352a127ca45eee35bcbb595
Jayson.Herbert <Jayson.Herbert@CHS004526LL>
2016-10-06 14:55:53 +0100

    Added in open all and search to regression output

--- CalculationCSharp/Areas/Configuration/Scripts/Regression/regressionOutputCtrl.js
+++ CalculationCSharp/Areas/Configuration/Scripts/Regression/regressionOutputCtrl.js
@@ -2,14 +2,23 @@
     
 
     function init() {
-
         $scope.output = angular.fromJson(Output);
-
     };
 
+    //UI
+    $scope.OpenAllButton = function () {
+        angular.forEach($scope.output, function (value, key, obj) {
+            $scope.openIndexRegression[key] = true;
+        })
+    }
+
+    $scope.CloseAllButton = function () {
+        angular.forEach($scope.openIndexRegression, function (value, key, obj) {
+            $scope.openIndexRegression[key] = false;
+        })
+    }
 
     $scope.SaveButtonClick = function getFormFields() {  //function that sets the parameters available under the different variable types     
-
         $uibModalInstance.close();
     }
 
