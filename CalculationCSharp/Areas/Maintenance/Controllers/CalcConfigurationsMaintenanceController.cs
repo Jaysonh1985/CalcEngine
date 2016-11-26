86697b7bbc6394904868b8166a0ee9f04c89d745
jayso <jayso@DESKTOP-498PN9N>
2016-11-26 17:51:47 +0000

    Ability to get JSON from calc configuration from data table in a maintenance function

---
+++ CalculationCSharp/Areas/Maintenance/Controllers/CalcConfigurationsMaintenanceController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CalculationCSharp.Models;
+
+namespace CalculationCSharp.Areas.Maintenance.Controllers
+{
+    public class CalcConfigurationsMaintenanceController : Controller
+    {
+        private CalculationDBContext db = new CalculationDBContext();
+
+        // GET: Maintenance/CalcConfigurationsMaintenance
+        public ActionResult Index()
+        {
+            return View(db.CalcConfiguration.ToList());
+        }
+
+        // GET: Maintenance/CalcConfigurationsMaintenance/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalcConfiguration calcConfiguration = db.CalcConfiguration.Find(id);
+            if (calcConfiguration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calcConfiguration);
+        }
+
+        // GET: Maintenance/CalcConfigurationsMaintenance/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maintenance/CalcConfigurationsMaintenance/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Scheme,Name,User,Configuration,UpdateDate,Version")] CalcConfiguration calcConfiguration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CalcConfiguration.Add(calcConfiguration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(calcConfiguration);
+        }
+
+        // GET: Maintenance/CalcConfigurationsMaintenance/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalcConfiguration calcConfiguration = db.CalcConfiguration.Find(id);
+            if (calcConfiguration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calcConfiguration);
+        }
+
+        // POST: Maintenance/CalcConfigurationsMaintenance/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Scheme,Name,User,Configuration,UpdateDate,Version")] CalcConfiguration calcConfiguration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(calcConfiguration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(calcConfiguration);
+        }
+
+        // GET: Maintenance/CalcConfigurationsMaintenance/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalcConfiguration calcConfiguration = db.CalcConfiguration.Find(id);
+            if (calcConfiguration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calcConfiguration);
+        }
+
+        // POST: Maintenance/CalcConfigurationsMaintenance/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CalcConfiguration calcConfiguration = db.CalcConfiguration.Find(id);
+            db.CalcConfiguration.Remove(calcConfiguration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
