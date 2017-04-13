7896d6ad3bf0882ef65cd25a0646a81c8ab0284a
jayson.herbert <nkjnkjd>
2017-04-13 16:32:04 +0100

    Array

--- CalculationCSharp/Areas/Configuration/Models/Actions/LogicFunctions.cs
+++ CalculationCSharp/Areas/Configuration/Models/Actions/LogicFunctions.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright (c) 2016 Project AIM
+using CalculationCSharp.Models.ArrayFunctions;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -14,18 +15,50 @@ namespace CalculationCSharp.Areas.Configuration.Models.Actions
         public string TrueValue { get; set; }
         public string FalseValue { get; set; }
 
+        public string Output(List<CategoryViewModel> jCategory, LogicFunctions bit, int GroupID, int ItemID)
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            ArrayBuildingFunctions ArrayBuilder = new ArrayBuildingFunctions();
+            string[] Input1parts = null;
+            string[] Input2parts = null;
+            //Returns array
+            Input1parts = ArrayBuilder.InputArrayBuilder(bit.Input1, jCategory, GroupID, ItemID);
+            Input2parts = ArrayBuilder.InputArrayBuilder(bit.Input2, jCategory, GroupID, ItemID);
+            string Output = null;
+            string OutputValue = null;
+            //Gets Max Length of array so loops through all values        
+            int MaxLength = ArrayBuilder.GetMaxLength(Input1parts, Input2parts);
+            int Counter = 0;
+            //Loop through the array to calculate each value in array
+            for (int i = 0; i < MaxLength; i++)
+            {
+                dynamic InputA = null;
+                dynamic InputB = null;
+                //Gets the current array to use in the loop
+                InputA = ArrayBuilder.GetArrayPart(Input1parts, Counter);
+                InputB = ArrayBuilder.GetArrayPart(Input2parts, Counter);
+                //Calculates the value required
+                OutputValue = Calculate(jCategory, InputA, InputB, bit, GroupID, ItemID);
+                Output = Output + OutputValue + "~";
+                Counter = Counter + 1;
+            }
+            Output = Output.Remove(Output.Length - 1);
+            return Convert.ToString(Output);
+        }
+
+
         /// <summary>Outputs where Logic is set on the row .
         /// <para>jCategory = the whole configuration which is required to do the variable replace</para>
         /// <para>bit = the logic to set</para>
         /// <para>GroupID = current Group ID</para>
         /// <para>ItemID = current row ID</para>
         /// </summary>
-        public string Output(List<CategoryViewModel> jCategory, LogicFunctions bit, int GroupID, int ItemID)
+        public string Calculate(List<CategoryViewModel> jCategory, dynamic Input1, dynamic Input2, LogicFunctions bit, int GroupID, int ItemID)
         {
             JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
             CalculationCSharp.Areas.Configuration.Models.ConfigFunctions Config = new CalculationCSharp.Areas.Configuration.Models.ConfigFunctions();
-            dynamic InputA = Config.VariableReplace(jCategory, bit.Input1, GroupID, ItemID);
-            dynamic InputB = Config.VariableReplace(jCategory, bit.Input2, GroupID, ItemID);
+            dynamic InputA = Config.VariableReplace(jCategory, Input1, GroupID, ItemID);
+            dynamic InputB = Config.VariableReplace(jCategory, Input2, GroupID, ItemID);
             dynamic TrueVal = Config.VariableReplace(jCategory, bit.TrueValue, GroupID, ItemID);
             dynamic FalseVal = Config.VariableReplace(jCategory, bit.FalseValue, GroupID, ItemID);
             //Parses Decimals
